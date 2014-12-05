1c7133a56f8288c706f93645c9221f3d8f6b639a
Adam Gralinski <agralinski@gmail.com>
2014-12-05 14:17:07 +0100

    SeminarFactory created

--- my_code/spec/SeminarSpec.js
+++ my_code/spec/SeminarSpec.js
@@ -1,9 +1,30 @@
 /**
  * Created by gralinsa on 2014-12-04.
  */
+var SeminarFactory = {
+    create: function(overwrites){
+        var defaults = {name:"JavaScript-Basics", price: 500};
+        var values = Object.extend(defaults, overwrites);
+        return Seminar.create(values.name, values.price);
+    }
+};
+Object.extend = function(obj, props){
+    for (var prop in props) { obj[prop] = props[prop];}
+    return obj;
+};
+
 describe('Seminar', function(){
     it('should have a name', function(){
-        Seminar.create('JavaScript-Basiss');
+        var seminar = SeminarFactory.create({name: 'JavaScript-Basics'});
+        expect(seminar.name).toEqual('JavaScript-Basics');
+    });
+    it('should have a price', function(){
+        var seminar = SeminarFactory.create({price: 49.99});
+        expect(seminar.netPrice()).toEqual(49.99);
+    });
+    it('should add 20% tax to price', function(){
+        var seminar = SeminarFactory.create({price: 100});
+        expect(seminar.grossPrice()).toEqual(120);
     });
 });
 
--- my_code/src/Seminar.js
+++ my_code/src/Seminar.js
@@ -1,12 +1,38 @@
 /**
  * Created by gralinsa on 2014-12-04.
  */
-;var Seminar = {
-    create: function(name){
-        return Object.create(Seminar).init(name);
+// freeze & seal
+// map, filter, reduce
+;
+var VAT_RATE = 1.2;
+var Seminar = {
+    _price: 0,
+    _name : null,
+    create: function(name, price){
+        return Object.create(Seminar).init(name, price);
+    },
+    init: function(name, price){
+        this.price = price;
+        this.name = name;
+        return this;
+    },
+    get price(){
+        return this._price;
+    },
+    set price(n){
+        return this._price = n;
+    },
+    get name(){
+        return this._name;
+    },
+    set name(n){
+        return this._name = n;
     },
 
-    init: function(name){
-        return this;
+    netPrice: function(){
+        return this.price;
+    },
+    grossPrice: function(){
+        return this.price * VAT_RATE;
     }
 };
